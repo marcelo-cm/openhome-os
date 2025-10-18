53513fd771a543c4f928f9bc805ebf26b35193f3
marcelo <marcechaman@gmail.com>
2025-10-17 22:01:46 -0700

    implement card demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardPanel,
-  CardTitle,
-} from '@/components/ui/card';
+import CardDemo from '@/components/card-demo';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center p-24">
-      <Card>
-        <CardHeader>
-          <CardTitle>Title</CardTitle>
-          <CardDescription>Description</CardDescription>
-        </CardHeader>
-        <CardPanel>Content</CardPanel>
-        <CardFooter>Footer</CardFooter>
-      </Card>
+      <CardDemo />
     </main>
   );
 }

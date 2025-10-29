2a01ff924cd07c8cf4884bb3c13315fae483b500
marcelo <marcechaman@gmail.com>
2025-10-29 09:31:58 -0700

    Fix build errors

--- apps/platform/app/(platform)/layout.tsx
+++ apps/platform/app/(platform)/layout.tsx
@@ -1,5 +1,3 @@
-import { Suspense } from 'react';
-
 import PlatformNavBar from './_components/platform-nav-bar';
 import PlatformProviders from './_layers/_providers/platform-providers';
 
@@ -9,15 +7,13 @@ export const dynamic = 'force-dynamic';
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Suspense fallback={<div className="flex min-h-screen w-dvw" />}>
-      <PlatformProviders>
-        <main className="flex min-h-screen w-dvw flex-col items-center">
-          <PlatformNavBar />
+    <PlatformProviders>
+      <main className="flex min-h-screen w-dvw flex-col items-center">
+        <PlatformNavBar />
 
-          <div className="flex w-full flex-row">{children}</div>
-        </main>
-      </PlatformProviders>
-    </Suspense>
+        <div className="flex w-full flex-row">{children}</div>
+      </main>
+    </PlatformProviders>
   );
 };
 
